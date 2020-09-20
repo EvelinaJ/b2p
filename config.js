e2550d69fb00458434c06b20c961bc2de8f8c270
Evelina Judeikytė <45915524+EvelinaJ@users.noreply.github.com>
2020-09-20 16:56:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -60,7 +60,7 @@ var config = {
         {
             id: 'story-intro-3',
             //title: 'People directly served at each site',
-            image: 'https://github.com/EvelinaJ/b2p_scrolly/blob/master/b2pbars.svg',
+            image: '../blob/master/b2pbars.svg',
             //image: 'images/b2pbars.jpg',
             //description: 'no text',
             location: {
